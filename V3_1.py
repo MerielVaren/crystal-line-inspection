4c4b6dc6c1ec419e7ff667de1c495dd80522392d
Shinki <2633915958@qq.com>
2021-12-10 11:35:57 +0800

    修复线段数据类型为numpy.inc带来的数值溢出异常,修复异常交点问题

--- V3_1.py
+++ V3_1.py
@@ -16,10 +16,12 @@ def lines_crossed(line1, line2, mode=1):
 
     x = y = 0
     x1, y1, x2, y2 = line1
+    line2 = [int(i) for i in line2]
     x3, y3, x4, y4 = line2
 
     if mode == 2:
-        if ((y3*(x2-x1)-((y2-y1)*(x3-x1) + y1*(x2-x1))) * (y4*(x2-x1)-((y2-y1)*(x4-x1) + y1*(x2-x1))) > 0):
+        # if ((y3*(x2-x1)-((y2-y1)*(x3-x1) + y1*(x2-x1))) * (y4*(x2-x1)-((y2-y1)*(x4-x1) + y1*(x2-x1))) > 0):
+        if ((x1 - x3) * (y2 - y3) - (y1 - y3) * (x2 - x3)) * ((x1 - x4) * (y2 - y3) - (y1 - y4) * (x2 - x4)) > 0:
             return False, (0, 0)
 
     if (x2 - x1) == 0:
